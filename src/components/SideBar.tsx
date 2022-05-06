c1ea1711d685a81127068ab4d01393bcf526ab34
Mauricio Nascimento de Oliveira <mauricio.nascimento@havan.com.br>
2022-05-06 17:40:21 -0300

    Resolve requisitos do desafio

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface GenryItemProps{ 
+  id: number,
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string
+}
+interface ListGenryProps{
+  genres: GenryItemProps[];
+  selectedGenreId: number;
+  setSelectedGenreId: (id: number) => void;
+}
+
+export function SideBar({genres, selectedGenreId, setSelectedGenreId}: ListGenryProps) {
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+
+  );
 }
